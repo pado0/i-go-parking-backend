5201cecd66646821be97cf02d046e909b9ba0515
pado0 <bgshhd95@gmail.com>
2025-03-23 21:49:05 -0700

    feat: delete social login feature

--- domain/src/main/kotlin/com/boilerplate/domain/SocialLoginAccount.kt
+++
@@ -1,5 +0,0 @@
-package com.boilerplate.domain
-
-data class SocialLoginAccount(
-    val email: String,
-)
--- port-out/persistence-port-out/src/main/kotlin/com/boilterplate/persistence/port/out/LoadSocialLoginAccount.kt
+++
@@ -1,7 +0,0 @@
-package com.boilterplate.persistence.port.out
-
-import com.boilerplate.domain.SocialLoginAccount
-
-interface LoadSocialLoginAccount {
-    fun findByEmail(email: String): SocialLoginAccount?
-}
